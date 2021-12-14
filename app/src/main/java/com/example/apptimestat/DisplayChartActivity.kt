d951709a361a187e5a95df8eb5d440405d4c4fd4
solskaklaudia <solska.klaudia@gmail.com>
2021-12-14 19:49:50 +0100

    displaying data

--- app/src/main/java/com/example/apptimestat/DisplayChartActivity.kt
+++ app/src/main/java/com/example/apptimestat/DisplayChartActivity.kt
@@ -3,6 +3,7 @@ package com.example.apptimestat
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,8 @@ import kotlinx.coroutines.withContext
 import lecho.lib.hellocharts.model.PieChartData
 import lecho.lib.hellocharts.model.SliceValue
 import lecho.lib.hellocharts.view.PieChartView
+import org.json.JSONArray
+import org.json.JSONTokener
 import retrofit2.Retrofit
 
 
@@ -30,29 +33,22 @@ class DisplayChartActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView2)
         textView.setText(getString(R.string.chart_title) + timePeriod.toString())
 
-        val pieChartView = findViewById<PieChartView>(R.id.chart)
-        val pieData: MutableList<SliceValue> = ArrayList()
+        val userId: String = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        var dateRange = 1
 
-        pieData += setSliceValue(15f, R.color.moonstoneBlue, "instagram")
-        pieData += setSliceValue(25f, R.color.ashGray, "messenger")
-        pieData += setSliceValue(10f, R.color.purpleLight, "tiktok")
-        pieData += setSliceValue(60f, R.color.spanishPink, "snapchat")
-        pieData += setSliceValue(70f, R.color.pearl, "youtube")
+        when {
+            timePeriod.toString() == "week" -> {
+                dateRange = 7
+            }
+            timePeriod.toString() == "month" -> {
+                dateRange = 30
+            }
+            timePeriod.toString() == "year" -> {
+                dateRange = 365
+            }
+        }
 
-        val pieChartData = PieChartData(pieData)
-
-        // labels
-        pieChartData.setHasLabels(true)
-        pieChartData.valueLabelTextSize = 14
-
-        pieChartData.setHasCenterCircle(true)
-        pieChartData.centerText1 = "App usage time"
-        pieChartData.centerText1FontSize = 20
-        pieChartData.centerText1Color = Color.parseColor("#0097A7")
-
-        pieChartView.pieChartData = pieChartData
-
-        getMethod()
+        getMethod(userId, dateRange)
 
     }
 
@@ -61,7 +57,7 @@ class DisplayChartActivity : AppCompatActivity() {
         return SliceValue(value, ContextCompat.getColor(context, color)).setLabel(label)
     }
 
-    fun getMethod() {
+    private fun getMethod(userId: String, dateRange: Int) {
 
         // Create Retrofit
         val retrofit = Retrofit.Builder()
@@ -71,10 +67,8 @@ class DisplayChartActivity : AppCompatActivity() {
         // Create Service
         val service = retrofit.create(APIService::class.java)
 
-        CoroutineScope(Dispatchers.IO).launch {
 
-            val userId = "xd"
-            val dateRange = 7
+        CoroutineScope(Dispatchers.IO).launch {
 
             // Do the GET request and get response
             val response = service.getData(userId, dateRange)
@@ -82,16 +76,60 @@ class DisplayChartActivity : AppCompatActivity() {
             withContext(Dispatchers.Main) {
                 if (response.isSuccessful) {
 
+                    val colors = listOf(
+                        R.color.moonstoneBlue,
+                        R.color.ashGray,
+                        R.color.purpleLight,
+                        R.color.spanishPink,
+                        R.color.pearl
+                    )
+
+                    val pieChartView = findViewById<PieChartView>(R.id.chart)
+                    val pieData: MutableList<SliceValue> = ArrayList()
+
                     // Convert raw JSON to pretty JSON using GSON library
                     val gson = GsonBuilder().setPrettyPrinting().create()
                     val prettyJson = gson.toJson(
                         JsonParser.parseString(
                             response.body()
-                                ?.string() // About this thread blocking annotation : https://github.com/square/retrofit/issues/3255
+                                ?.string()
                         )
                     )
 
                     Log.d("Pretty Printed JSON :", prettyJson)
+
+                    val jsonArray = JSONTokener(prettyJson).nextValue() as JSONArray
+                    for (i in 0 until jsonArray.length()) {
+                        if (i < 5) {
+                            // Average time
+                            val averageTime = jsonArray.getJSONObject(i).getString("average_time")
+                            Log.i("Average time: ", averageTime)
+
+                            // Comparison with previous period
+                            val comparison = jsonArray.getJSONObject(i).getString("comparison")
+                            Log.i("Comparison: ", comparison)
+
+                            // Application name
+                            val name = jsonArray.getJSONObject(i).getString("name")
+                            Log.i("Application name: ", name)
+
+
+                            pieData += setSliceValue(averageTime.toFloat(), colors.random(), name)
+                        }
+                    }
+
+                    val pieChartData = PieChartData(pieData)
+
+                    // labels
+                    pieChartData.setHasLabels(true)
+                    pieChartData.valueLabelTextSize = 14
+
+                    pieChartData.setHasCenterCircle(true)
+                    pieChartData.centerText1 = "App usage time"
+                    pieChartData.centerText1FontSize = 20
+                    pieChartData.centerText1Color = Color.parseColor("#0097A7")
+
+                    pieChartView.pieChartData = pieChartData
 
                 } else {
 
--- app/src/main/java/com/example/apptimestat/DisplayTableActivity.kt
+++ app/src/main/java/com/example/apptimestat/DisplayTableActivity.kt
@@ -1,8 +1,19 @@
 package com.example.apptimestat
 
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONTokener
+import retrofit2.Retrofit
 
 class DisplayTableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +23,133 @@ class DisplayTableActivity : AppCompatActivity() {
         val timePeriod = intent.extras?.get(EXTRA_TIME)
         val textView = findViewById<TextView>(R.id.textView3)
         textView.setText(getString(R.string.table_title) + timePeriod.toString())
+
+        val userId: String = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        var dateRange = 1
+
+        when {
+            timePeriod.toString() == "week" -> {
+                dateRange = 7
+            }
+            timePeriod.toString() == "month" -> {
+                dateRange = 30
+            }
+            timePeriod.toString() == "year" -> {
+                dateRange = 365
+            }
+        }
+
+        getMethod(userId, dateRange)
+    }
+
+    fun getMethod(userId: String, dateRange: Int) {
+
+        // Create Retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://apptimestat.eu.pythonanywhere.com")
+            .build()
+
+        // Create Service
+        val service = retrofit.create(APIService::class.java)
+
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            // Do the GET request and get response
+            val response = service.getData(userId, dateRange)
+
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+
+
+                    val name1 = findViewById<TextView>(R.id.name1)
+                    val name2 = findViewById<TextView>(R.id.name2)
+                    val name3 = findViewById<TextView>(R.id.name3)
+                    val name4 = findViewById<TextView>(R.id.name4)
+                    val name5 = findViewById<TextView>(R.id.name5)
+                    val name6 = findViewById<TextView>(R.id.name6)
+                    val name7 = findViewById<TextView>(R.id.name7)
+                    val name8 = findViewById<TextView>(R.id.name8)
+                    val name9 = findViewById<TextView>(R.id.name9)
+                    val name10 = findViewById<TextView>(R.id.name10)
+
+                    val avg1 = findViewById<TextView>(R.id.avg1)
+                    val avg2 = findViewById<TextView>(R.id.avg2)
+                    val avg3 = findViewById<TextView>(R.id.avg3)
+                    val avg4 = findViewById<TextView>(R.id.avg4)
+                    val avg5 = findViewById<TextView>(R.id.avg5)
+                    val avg6 = findViewById<TextView>(R.id.avg6)
+                    val avg7 = findViewById<TextView>(R.id.avg7)
+                    val avg8 = findViewById<TextView>(R.id.avg8)
+                    val avg9 = findViewById<TextView>(R.id.avg9)
+                    val avg10 = findViewById<TextView>(R.id.avg10)
+
+                    val comp1 = findViewById<TextView>(R.id.comp1)
+                    val comp2 = findViewById<TextView>(R.id.comp2)
+                    val comp3 = findViewById<TextView>(R.id.comp3)
+                    val comp4 = findViewById<TextView>(R.id.comp4)
+                    val comp5 = findViewById<TextView>(R.id.comp5)
+                    val comp6 = findViewById<TextView>(R.id.comp6)
+                    val comp7 = findViewById<TextView>(R.id.comp7)
+                    val comp8 = findViewById<TextView>(R.id.comp8)
+                    val comp9 = findViewById<TextView>(R.id.comp9)
+                    val comp10 = findViewById<TextView>(R.id.comp10)
+
+                    val fields = listOf(
+                        listOf(name1, avg1, comp1),
+                        listOf(name2, avg2, comp2),
+                        listOf(name3, avg3, comp3),
+                        listOf(name4, avg4, comp4),
+                        listOf(name5, avg5, comp5),
+                        listOf(name6, avg6, comp6),
+                        listOf(name7, avg7, comp7),
+                        listOf(name8, avg8, comp8),
+                        listOf(name9, avg9, comp9),
+                        listOf(name10, avg10, comp10)
+                    )
+
+                    // Convert raw JSON to pretty JSON using GSON library
+                    val gson = GsonBuilder().setPrettyPrinting().create()
+                    val prettyJson = gson.toJson(
+                        JsonParser.parseString(
+                            response.body()
+                                ?.string()
+                        )
+                    )
+
+                    Log.d("Pretty Printed JSON :", prettyJson)
+
+                    val jsonArray = JSONTokener(prettyJson).nextValue() as JSONArray
+                    for (i in 0 until jsonArray.length()) {
+                        if (i < 10) {
+
+                            // Average time
+                            val averageTime =
+                                jsonArray.getJSONObject(i).getString("average_time")
+                            Log.i("Average time: ", averageTime)
+
+                            // Comparison with previous period
+                            val comparison = jsonArray.getJSONObject(i).getString("comparison")
+                            Log.i("Comparison: ", comparison)
+
+                            // Application name
+                            val name = jsonArray.getJSONObject(i).getString("name")
+                            Log.i("Application name: ", name)
+
+                            fields[i][0].setText(name)
+                            fields[i][1].setText(averageTime)
+                            fields[i][2].setText(comparison)
+
+                        }
+                    }
+
+                } else {
+
+                    Log.e("RETROFIT_ERROR", response.code().toString())
+
+                }
+            }
+        }
     }
 
 }
